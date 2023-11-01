b9fb6eb74d4eb8726662a547b164a8ef1c68b60e
LUKE REYNOLDS <luke.reynolds.92@gmail.com>
2023-11-01 21:48:44 +0000

    add error messages to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useState } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 const API_KEY = import.meta.env.VITE_API_KEY;
@@ -7,6 +7,7 @@ const API_KEY = import.meta.env.VITE_API_KEY;
 function App() {
   const [location, setLocation] = useState({});
   const [search, setSearch] = useState("");
+  const [error, setError] = useState(null);
 
   function handleChange(event) {
     setSearch(event.target.value);
@@ -14,9 +15,20 @@ function App() {
 
   async function getLocation(event) {
     event.preventDefault();
+    if (!search) {
+      setError("Please enter a valid location.");
+      return;
+    }
+
     const API = `https://eu1.locationiq.com/v1/search?q=${search}&key=${API_KEY}&format=json`;
-    const res = await axios.get(API);
-    setLocation(res.data[0]);
+
+    try {
+      const res = await axios.get(API);
+      setLocation(res.data[0]);
+      setError(null);
+    } catch (error) {
+      setError("An error occurred while fetching data.");
+    }
   }
 
   return (
@@ -27,15 +39,19 @@ function App() {
           <input onChange={handleChange} placeholder="Location" />
           <button className="explore-btn">Explore!</button>
         </form>
+        {error && <p className="error-message">{error}</p>}
       </div>
-      <div className="display">
-        <p>Longitude: {location.lon}</p>
-        <p>Latitude: {location.lat}</p>
-        <h2>Location: {location.display_name}</h2>
-        <img
-          src={`https://maps.locationiq.com/v3/staticmap?key=${API_KEY}&center=${location.lat},${location.lon}&zoom=13&format=png`}
-        />
-      </div>
+      {location && !error && (
+        <div className="display">
+          <p>Longitude: {location.lon}</p>
+          <p>Latitude: {location.lat}</p>
+          <h2>Location: {location.display_name}</h2>
+          <img
+            src={`https://maps.locationiq.com/v3/staticmap?key=${API_KEY}&center=${location.lat},${location.lon}&zoom=13&format=png`}
+            alt="Map"
+          />
+        </div>
+      )}
     </main>
   );
 }
